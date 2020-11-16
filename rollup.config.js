75efac078180499476b594e8982bc5466e5c02a7
Adam Rackis <adamrackis@gmail.com>
2020-11-15 22:02:47 -0600

    Remove built assets. Tweak output name.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import svelte from "rollup-plugin-svelte";
 const path = require("path");
 
-const getConfig = ({ file }) => ({
+const getConfig = ({ file, output }) => ({
   input: `./src/${file}`,
   output: {
     format: "esm",
-    file,
+    file: output || file,
   },
   plugins: [svelte({})],
   external: ["svelte", "svelte/internal", "svelte/store", "svelte/easing", "svelte/transition", "svelte-helpers/animation"]
 });
 
-module.exports = [getConfig({ file: "spring-transitions.js" }), getConfig({ file: "Modal.svelte" })];
+module.exports = [getConfig({ file: "spring-transitions.js" }), getConfig({ file: "Modal.svelte", output: "Modal.js" })];
