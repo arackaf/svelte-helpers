127a144299be30e0976584e5f732bc822695c3b7
Adam Rackis <adamrackis@gmail.com>
2020-11-26 13:43:15 -0600

    auto-suggest work

--- src/AutoSuggest.svelte
+++ src/AutoSuggest.svelte
@@ -10,7 +10,6 @@
 
   function onBlur() {
     open = false;
-    currentSearch = "";
   }
 
   function filterOptions() {
@@ -26,8 +25,8 @@
 <style>
   :root {
     --svelte-helpers-auto-complete-border-color: lightgray;
-    --svelte-helpers-auto-complete-container-border-radius: 4px;
-    --svelte-helpers-auto-complete-container-offset-for-border-radius: 3px;
+    --svelte-helpers-auto-complete-border-width: 1px;
+    --svelte-helpers-auto-complete-border-radius: 4px;
     --svelte-helpers-auto-complete-item-padding: 5px;
     --svelte-helpers-auto-complete-item-hover-background: lightgray;
     --svelte-helpers-auto-complete-item-hover-cursor: pointer;
@@ -39,12 +38,15 @@
   }
 
   input {
-    border-top: 0;
-    border-left: 0;
-    border-right: 0;
-    border-bottom: 1px solid var(--svelte-helpers-auto-complete-border-color);
+    border: var(--svelte-helpers-auto-complete-border-width) solid var(--svelte-helpers-auto-complete-border-color);
+    border-radius: 5px;
     padding: 5px;
     outline: none;
+  }
+
+  :global(input.open) {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
   }
 
   .items-root {
@@ -52,12 +54,14 @@
   }
 
   .items-container {
-    min-width: calc(100% + var(--svelte-helpers-auto-complete-container-offset-for-border-radius));
-    border: 1px solid var(--svelte-helpers-auto-complete-border-color);
-    border-radius: var(--svelte-helpers-auto-complete-container-border-radius);
+    min-width: calc(100% - 2 * var(--svelte-helpers-auto-complete-border-width));
+    border: var(--svelte-helpers-auto-complete-border-width) solid var(--svelte-helpers-auto-complete-border-color);
+    border-radius: var(--svelte-helpers-auto-complete-border-radius);
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
     position: absolute;
-    left: calc(-1 * var(--svelte-helpers-auto-complete-container-offset-for-border-radius));
-    top: -1px;
+    left: 0;
+    top: calc(-1 * var(--svelte-helpers-auto-complete-border-width));
     max-height: 200px;
     overflow: hidden;
   }
@@ -81,7 +85,7 @@
 </style>
 
 <div class="root" class:open>
-  <input {placeholder} bind:value={currentSearch} on:focus={() => (open = true)} on:blur={onBlur} />
+  <input {placeholder} bind:value={currentSearch} on:focus={() => (open = true)} on:blur={onBlur} class:open />
   {#if open}
     <div transition:fade={{ duration: 150 }} class="items-root">
       <div class="items-container">
