97405a9ffffb38fdedb55a69c78b19f787b04e67
Adam Rackis <adamrackis@gmail.com>
2020-11-26 14:56:00 -0600

    auto-suggest work

--- src/AutoSuggest.svelte
+++ src/AutoSuggest.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { spring } from "svelte/motion";
   import { fade, slide } from "svelte/transition";
   let open = false;
 
@@ -10,16 +11,44 @@
 
   function onBlur() {
     open = false;
+    currentSearch = "";
   }
 
   function filterOptions() {
     return options.filter(item => new RegExp(currentSearch, "i").test(item[filterField]));
   }
+
   $: {
     filteredOptions = currentSearch ? filterOptions() : options;
   }
 
-  const test = () => void 0;
+  let animateContainerHeight = false;
+  const slideInSpring = spring(0, { stiffness: 0.1, damping: 0.2 });
+
+  function setSpringHeight(height, hard) {
+    let maxHeightVar = getComputedStyle(document.documentElement).getPropertyValue("--svelte-helpers-auto-complete-results-max-height");
+    let maxHeight = parseInt(maxHeightVar, 10);
+
+    slideInSpring.set(Math.min(resultsList.offsetHeight, maxHeight), hard ? { hard: true } : void 0);
+  }
+
+  let itemsHeightObserver = new ResizeObserver(([{ contentRect }]) => {
+    setSpringHeight(contentRect.height);
+  });
+  let resultsList;
+
+  function opening() {}
+  function opened() {
+    setSpringHeight(resultsList.offsetHeight, true);
+
+    animateContainerHeight = true;
+    itemsHeightObserver.observe(resultsList);
+  }
+  function closing() {
+    itemsHeightObserver.unobserve(resultsList)
+    animateContainerHeight = false;
+  }
+  function closed() {}
 </script>
 
 <style>
@@ -28,6 +57,7 @@
     --svelte-helpers-auto-complete-border-width: 1px;
     --svelte-helpers-auto-complete-border-radius: 4px;
     --svelte-helpers-auto-complete-item-padding: 5px;
+    --svelte-helpers-auto-complete-results-max-height: 200px;
     --svelte-helpers-auto-complete-item-hover-background: lightgray;
     --svelte-helpers-auto-complete-item-hover-cursor: pointer;
   }
@@ -63,7 +93,7 @@
     left: 0;
     top: calc(-1 * var(--svelte-helpers-auto-complete-border-width));
     max-height: 200px;
-    overflow: hidden;
+    overflow: auto;
   }
 
   ul {
@@ -84,12 +114,20 @@
   }
 </style>
 
+<h1>{$slideInSpring}</h1>
+
 <div class="root" class:open>
   <input {placeholder} bind:value={currentSearch} on:focus={() => (open = true)} on:blur={onBlur} class:open />
   {#if open}
-    <div transition:fade={{ duration: 150 }} class="items-root">
-      <div class="items-container">
-        <ul>
+    <div
+      transition:fade={{ duration: 150 }}
+      class="items-root"
+      on:introstart={opening}
+      on:introend={opened}
+      on:outrostart={closing}
+      on:outroend={closed}>
+      <div style="height: {animateContainerHeight ? $slideInSpring + 'px' : 'auto'}" class="items-container">
+        <ul bind:this={resultsList}>
           {#each filteredOptions as option (option.name)}
             <li class="result">
               <slot name="result" {option}>Hello World FOO Hello World Hello World Hello World Hello World</slot>
