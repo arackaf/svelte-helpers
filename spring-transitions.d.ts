3270777f8f8e1bbd77ab141cb9ce402100b89760
Adam Rackis <adamrackis@hotmail.com>
2023-03-11 21:46:45 -0600

    Add typings

---
+++ spring-transitions.d.ts
@@ -0,0 +1,5 @@
+export function springIn(from: any, to: any, springOptions: any);
+
+export function springOut(from: any, to: any, springOptions: any);
+
+export function springFrames(from: any, to: any, opts: any);
